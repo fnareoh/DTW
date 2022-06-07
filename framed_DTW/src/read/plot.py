4db16345d980563cfcd728a3aa272446b9024693
fnareoh <garance.gourdel@laposte.net>
2022-06-07 19:01:19 +0200

    Rework the pipeline

--- framed_DTW/src/read/plot.py
+++ framed_DTW/src/read/plot.py
@@ -21,11 +21,25 @@ def parse_csv(filename):
         reader_csv = csv.reader(file)
         reader = [row for row in reader_csv]
         details = reader[0]
-        assert details[4] == "proba insertion Deletion Substitution"
-        output["IDS"] = float(details[5])
-        assert details[6] == "proba Homopolymer extension"
-        output["H"] = float(details[7])
-        output["IDS_distrib"] = details[8]
+        assert details[4] == "percentage of Indel"
+        output["ID"] = float(details[5])
+        assert details[6] == "percentage of SNP"
+        output["SNP"] = float(details[7])
+        assert details[8] == "proba Homopolymer extension"
+        output["H"] = float(details[9])
+        assert details[10] == "proba substitution sequencing"
+        output["seq_S"] = float(details[11])
+        assert (
+            reader[1][1] == "biological_var"
+            and reader[1][2] == "seq_S"
+            and reader[1][3] == "seq_H"
+        )
+        output["biological_var"] = [int(row[1]) for row in reader[2:]]
+        output["mean_bio"], output["stdev_bio"] = mean(output["biological_var"]), stdev(
+            output["biological_var"]
+        )
+        output["seq_S"] = [int(row[2]) for row in reader[2:]]
+        output["nb_homopoly"] = [int(row[3]) for row in reader[2:]]
         assert reader[1][-1] == "min ED" and reader[1][-2] == "min DTW"
         output["ED"] = [int(row[-1]) for row in reader[2:]]
         output["DTW"] = [int(row[-2]) for row in reader[2:]]
@@ -33,73 +47,88 @@ def parse_csv(filename):
         output["mean_DTW"], output["stdev_DTW"] = mean(output["DTW"]), stdev(
             output["DTW"]
         )
-        assert reader[1][1] == "nb_IDS" and reader[1][2] == "nb_H"
-        output["nb_IDS"] = [int(row[1]) for row in reader[2:]]
-        output["mean_IDS"], output["stdev_IDS"] = mean(output["nb_IDS"]), stdev(
-            output["nb_IDS"]
-        )
-        output["nb_H"] = [int(row[2]) for row in reader[2:]]
     return output
 
 
+def minus_bio_var(output):
+    output["DTW"] = [
+        output["DTW"][i] - output["biological_var"][i]
+        for i in range(len(output["DTW"]))
+    ]
+    output["ED"] = [
+        output["ED"][i] - output["biological_var"][i] for i in range(len(output["DTW"]))
+    ]
+
+
 def parse_args():
-    other = {"IDS": "H", "H": "IDS"}
-    param = {"IDS": "*", "H": "*"}
+    other = {"ID": "H", "H": "ID"}
+    param = {"ID": "*", "H": "*", "SNP": 1}
     param["basename"] = argv[1]
     tmp_parse = param["basename"].split("_")
     assert tmp_parse[-2] == "N"
     param["N"] = int(tmp_parse[-1])
     param["fixed"] = argv[2]
-    assert param["fixed"] in ["IDS", "H"]
+    assert param["fixed"] in ["ID", "H"]
     param["varying"] = other[param["fixed"]]
-    param[param["fixed"]] = float(argv[3])
-    param["IDS_distrib"] = None
+    param[param["fixed"]] = round(float(argv[3]), 2)
 
     # Get values
     x = []
-    y_mean = {"DTW": [], "ED": [], "IDS": []}
-    y_stdev = {"DTW": [], "ED": [], "IDS": []}
-    all_values = {"IDS": [], "ED": [], "DTW": []}
+    y_mean = {"DTW": [], "ED": [], "bio": []}
+    y_stdev = {"DTW": [], "ED": [], "bio": []}
+    all_values = {"biological_var": [], "nb_homopoly": [], "ED": [], "DTW": []}
     for name in glob(
-        f"{param['basename']}_IDS_{param['IDS']}_H_{param['H']}_IDS_distrib_*.csv"
+        f"{param['basename']}_ID_{param['ID']}_SNP_{param['SNP']}_H_{param['H']}_seqS_0.001*.csv"
     ):
         output = parse_csv(name)
-
-        if (
-            param["IDS_distrib"] is None
-            or param["IDS_distrib"] == output["IDS_distrib"]
-        ):
-            param["IDS_distrib"] = output["IDS_distrib"]
-            x.append(output[param["varying"]])
-            all_values["IDS"] += output["nb_IDS"]
-            all_values["ED"] += output["ED"]
-            all_values["DTW"] += output["DTW"]
-            y_mean["DTW"].append(output["mean_DTW"])
-            y_stdev["DTW"].append(output["stdev_DTW"])
-            y_mean["ED"].append(output["mean_ED"])
-            y_stdev["ED"].append(output["stdev_ED"])
-            y_mean["IDS"].append(output["mean_IDS"])
-            y_stdev["IDS"].append(output["stdev_IDS"])
+        # minus_bio_var(output)  # Deducts the bio distance !
+        x.append(output[param["varying"]])
+        all_values["biological_var"] += output["biological_var"]
+        all_values["nb_homopoly"] += output["nb_homopoly"]
+        all_values["ED"] += output["ED"]
+        all_values["DTW"] += output["DTW"]
+        y_mean["DTW"].append(output["mean_DTW"])
+        y_stdev["DTW"].append(output["stdev_DTW"])
+        y_mean["ED"].append(output["mean_ED"])
+        y_stdev["ED"].append(output["stdev_ED"])
+        y_mean["bio"].append(output["mean_bio"])
+        y_stdev["bio"].append(output["stdev_bio"])
 
     x, y_mean, y_stdev = sorted(x), reorder(x, y_mean), reorder(x, y_stdev)
     plot_avg_mean_stdev(param, x, y_mean, y_stdev)
     plt.figure().clear()
 
-    x = all_values["IDS"]
+    print(
+        f"Average ratio DTW/bio_dist: {mean([all_values['DTW'][i]/(all_values['biological_var'][i]+1) for i in range(len(all_values['DTW']))])}"
+    )
+    print(
+        f"Stdev ratio DTW/bio_dist: {stdev([all_values['DTW'][i]/(all_values['biological_var'][i]+1) for i in range(len(all_values['DTW']))])}"
+    )
+    print(
+        f"Average ratio ED/bio_dist: {mean([all_values['ED'][i]/(all_values['biological_var'][i]+1) for i in range(len(all_values['DTW']))])}"
+    )
+    print(
+        f"Average ratio ED/bio_dist: {mean([all_values['ED'][i]/(all_values['biological_var'][i]+1) for i in range(len(all_values['DTW']))])}"
+    )
+    x = all_values["nb_homopoly"]
+    all_values.pop("biological_var", None)
     x, y = sorted(x), reorder(x, all_values)
     simple_plot(param, x, y)
 
 
 def simple_plot(param, x, y):
     x_legend = {
-        "IDS": f"Number of  IN, DEL or SUB error with repartition {param['IDS_distrib']}",
+        "H": "Number of homopolymer extension",
+        "bio": f"Number of  IN, DEL or SUB with repartition ",
     }
     for k in y:
         plt.plot(x, y[k], label=k)
-    plt.ylabel("Distance")
+    # plt.plot(x, [1] * len(x), color="black")
+    plt.ylabel("Distance - biological variation")
     plt.xlabel(x_legend[param["varying"]])
+    # plt.yscale("log")
     plt.title(
-        f"Edit and dynamic time warping distance as a function\n of the number of insertion deletion and substitution"
+        f"Edit and dynamic time warping distance as a function\n of the number of homopolymer extensions"
     )
     plt.legend(loc="upper left")
     plt.savefig(
@@ -109,19 +138,21 @@ def simple_plot(param, x, y):
 
 def plot_avg_mean_stdev(param, x, y_mean, y_stdev):
     x_legend = {
-        "IDS": "Probability of IN, DEL or SUB error",
+        "ID": "Probability of IN, DEL or SUB error",
         "H": "Probability of homopolymer extension error",
     }
     for k in y_mean:
-        plt.errorbar(x, y_mean[k], y_stdev[k], label=k, fmt="-o")
-    plt.ylabel("Average distance")
+        # plt.plot(x, y_mean[k], label=k)
+        if k != "bio":
+            plt.errorbar(x, y_mean[k], y_stdev[k], label=k, fmt="-o")
+    plt.ylabel("Average (distance - biological variation)")
     plt.xlabel(x_legend[param["varying"]])
-    plt.title(
-        f"Average distance for fixed {x_legend[param['fixed']]}\n equal to {param[param['fixed']]}, each point averaged over {param['N']} sequences"
-    )
+    # plt.title(
+    #    f"Average distance for fixed {x_legend[param['fixed']]}\n equal to {param[param['fixed']]}, each point averaged over {param['N']} sequences"
+    # )
     plt.legend(loc="upper left")
     plt.savefig(
-        f"{param['basename']}_fixed_{param['fixed']}_{param[param['fixed']]}_IDS_distrib_{param['IDS_distrib']}.png"
+        f"{param['basename']}_fixed_{param['fixed']}_{param[param['fixed']]}.png"
     )
 
 
--- framed_DTW/src/read/read_generator.py
+++ framed_DTW/src/read/read_generator.py
@@ -1,4 +1,4 @@
-from experiment import compute_matrices, print_details, reverse, complement
+from dynamic_prog.local_alignement import LocalED, LocalDTW
 from sys import argv
 import argparse
 from random import randint, random, choice, choices
@@ -6,68 +6,95 @@ import csv
 
 
 class Error_rate:
-    def __init__(self, ed, hom, ids):
-        self.inser_del_sub = ed
-        self.inser = ids[0]
-        self.dele = ids[1]
-        self.sub = ids[2]
+    def __init__(self, hom):
+        self.snp = 1  # percentage of substitution
+        self.indel = 0.05  # percentage of indel
+        self.max_len_ID = 10
+        self.seq_S = 0.001
         self.homopoly = hom
 
     def IDS_distrib(self):
         return f"I:{round(self.inser,2)},D:{round(self.dele,2)},S:{round(self.sub,2)}"
 
 
-def evaluate_dtw_ed(R, G, pos, qual):
-    ldtw, ldtw_rev_comp, led, led_rev_comp = compute_matrices(R, G)
+def evaluate_dtw_ed(R, G, pos, qual, biological_var, bio_qual):
+    """ Alligns the sequence R to G w.r. to ED and DTW """
+    ldtw = LocalDTW(R, G)
+    led = LocalED(R, G)
     score_dtw, pos_dtw = ldtw.min_last_row_val_index()
-    start_dtw, _, _ = ldtw.compute_origin_min_position()
-    score_dtw_rev_comp, pos_dtw_rev_comp = ldtw_rev_comp.min_last_row_val_index()
-    if score_dtw_rev_comp < score_dtw:
-        score_dtw, pos_dtw = score_dtw_rev_comp, pos_dtw_rev_comp
-        start_dtw, _, _ = ldtw_rev_comp.compute_origin_min_position()
+    start_dtw, al_P, al_T = ldtw.compute_origin_min_position()
 
     score_ed, pos_ed = led.min_last_row_val_index()
     start_ed, _, _ = led.compute_origin_min_position()
-    score_ed_rev_comp, pos_ed_rev_comp = led_rev_comp.min_last_row_val_index()
-    if score_ed_rev_comp < score_ed:
-        score_ed, pos_ed = score_ed_rev_comp, pos_ed_rev_comp
-        start_ed, _, _ = led_rev_comp.compute_origin_min_position()
 
-    if abs(pos - start_dtw) > 2 or abs(pos - start_ed) > 2:
+    if False:  # ugly debuging
         print("************* Read **************")
         print(f"starting position: {pos}")
+        print(f"biological_var: {biological_var}")
         print(R)
+        print(bio_qual)
         print(qual)
-        print_details(R, G, ldtw, ldtw_rev_comp, led, led_rev_comp)
+        print("******** Dynamic Time Warp **********")
+        print(
+            f"Alignement (cost {score_dtw}) of Q ending at {pos_dtw} in T starts at {start_dtw} in T."
+        )
+        print(al_P)
+        print(al_T)
+        print("******** Edit distance **********")
+        print(
+            f"Alignement (cost {score_ed}) of Q ending at {pos_ed} in T starts at {start_ed} in T."
+        )
 
     return score_dtw, score_ed
 
 
-def add_err(S, err):
+def add_IDS_err(S, err):
+    """ Adds Insertion Deletion and Substitution meant to represent a biological variant """
+    nucleotides = ["A", "C", "G", "T"]
+    list_S = []
+    list_qual = []
+    nb_IDS = 0
+    i = 0
+    while i < len(S):
+        c = S[i]
+        if random() < err.snp / 100:
+            nb_IDS += 1
+            c = choice([x for x in nucleotides if x != c])
+            list_qual.append("S")
+        elif random() < err.indel / 100:
+            err_type = choice(["insertion", "deletion"])
+            if err_type == "insertion":
+                len_i = randint(1, err.max_len_ID)
+                nb_IDS += len_i
+                tmp_c = choices(nucleotides, k=len_i)
+                list_S += tmp_c
+                list_qual.append("I" * len_i)
+            if err_type == "deletion":
+                len_d = randint(1, err.max_len_ID)
+                nb_IDS += len_d
+                c = ""
+                i += len_d - 1
+                list_qual.append("D" * len_d)
+        else:
+            list_qual.append("_")
+        list_S.append(c)
+        i += 1
+    return "".join(list_S), "".join(list_qual), nb_IDS
+
+
+def add_seq_err(S, err):
+    """ Adds Substitution and Homopolymer extension meant to represent sequencing errors """
     nucleotides = ["A", "C", "G", "T"]
     list_read = []
     list_qual = []
     last_c = ""
-    nb_IDS = 0
+    nb_S = 0
     nb_homopoly = 0
     for c in S:
-        if random() < err.inser_del_sub:
-            err_type = choices(
-                ["substitution", "insertion", "deletion"],
-                weights=[err.sub, err.inser, err.dele],
-                k=1,
-            )[0]
-            nb_IDS += 1
-            if err_type == "substitution":
-                c = choice([x for x in nucleotides if x != c])
-                list_qual.append("S")
-            if err_type == "insertion":
-                tmp_c = choice(nucleotides)
-                list_read.append(tmp_c)
-                list_qual.append("I")
-            if err_type == "deletion":
-                c = ""
-                list_qual.append("D")
+        if random() < err.seq_S:
+            nb_S += 1
+            c = choice([x for x in nucleotides if x != c])
+            list_qual.append("S")
         else:
             list_qual.append("_")
         list_read.append(c)
@@ -77,13 +104,44 @@ def add_err(S, err):
                 list_qual.append("H")
                 nb_homopoly += 1
         last_c = c
-    return "".join(list_read), "".join(list_qual), nb_IDS, nb_homopoly
+    return "".join(list_read), "".join(list_qual), nb_S, nb_homopoly
 
 
-def generate_read(G, N, read_length, err):
-    prob_rev_complement = 0.5
+def deletion_before(qual):
+    """ Compute for each position i the number of deletion in qual[:i] """
+    dele_prec = [0] * len(qual)
+    nb_del = 0
+    for i, c in enumerate(qual):
+        dele_prec[i] = nb_del
+        if c == "D":
+            nb_del += 1
+    return dele_prec
+
+
+def theoretical_distance(r, l, qual, dele_prec):
+    nb_match = qual[
+        r + dele_prec[r] : min(r + l + dele_prec[r + l - 1], len(qual))
+    ].count("_")
+    nb_mismatch = (
+        min(r + l + dele_prec[r + l], len(qual)) - (r + dele_prec[r]) - nb_match
+    )
+    if nb_mismatch > 30:
+        print(
+            "start:",
+            r + dele_prec[r + 1],
+            "end:",
+            r + l + dele_prec[r + l],
+            "len(qual):",
+            len(qual),
+        )
+        print("nb match:", nb_match, "nb nb_mismatch:", nb_mismatch)
+    return nb_mismatch
+
+
+def generate_read(Go, Gi, qual_Gi, N, read_length, err):
+    dele_prec = deletion_before(qual_Gi)
     genome_name = argv[1].split("/")[-1].split(".")[0]
-    output_file_name = f"results/{genome_name}_N_{N}_IDS_{err.inser_del_sub}_H_{err.homopoly}_IDS_distrib_{err.IDS_distrib()}"
+    output_file_name = f"results/{genome_name}_N_{N}_ID_{err.indel}_SNP_{err.snp}_H_{err.homopoly}_seqS_{err.seq_S}_max_len_indel_{err.max_len_ID}"
     read_file = open(output_file_name + ".fastq", "w")
     output_file = open(output_file_name + ".csv", "w")
     csv_output = csv.writer(output_file)
@@ -93,30 +151,44 @@ def generate_read(G, N, read_length, err):
             output_file_name,
             "Nb subseq",
             N,
-            "proba insertion Deletion Substitution",
-            err.inser_del_sub,
+            "percentage of Indel",
+            err.indel,
+            "percentage of SNP",
+            err.snp,
             "proba Homopolymer extension",
             err.homopoly,
-            err.IDS_distrib(),
+            "proba substitution sequencing",
+            err.seq_S,
         ]
     )
-    csv_output.writerow(["read_id", "nb_IDS", "nb_H", "min DTW", "min ED"])
+    csv_output.writerow(
+        ["read_id", "biological_var", "seq_S", "seq_H", "min DTW", "min ED"]
+    )
     for i in range(N):
-        r = randint(0, len(G) - read_length - 1)
-        subseq = G[r : r + read_length]
-        if random() < prob_rev_complement:
-            subseq = reverse(complement(subseq))
-        read, qual, nb_ids, nb_homopoly = add_err(subseq, err)
+        r = randint(0, len(Gi) - read_length - 1)
+        biological_var = theoretical_distance(r, read_length, qual_Gi, dele_prec)
+        subseq = Gi[r : r + read_length]
+        read, qual, nb_s, nb_homopoly = add_seq_err(subseq, err)
         id = (
             f"read_nb_{i}_pos_{r}_length_{read_length}_"
-            + f"homopoly_err_{err.homopoly}_err_inser_del_sub_{err.inser_del_sub}_"
-            + f"nb_H_{nb_homopoly}_nb_IDS_{nb_ids}_"
-            + f"IDS_distrib_{err.IDS_distrib()}"
+            + f"homopoly_err_{err.homopoly}_sub_err_{err.seq_S}_"
+            + f"Indel_{err.indel}_SNP_{err.snp}"
         )
-        read_file.write(f"@{id}\n{read}\n+{id}\n{qual}\n")
+        bio_qual = qual_Gi[
+            r
+            + dele_prec[r + 1] : min(
+                r + read_length + dele_prec[r + read_length - 1], len(qual_Gi)
+            )
+        ]
+        read_file.write(f"@{id}\n{read}\n+{id}\n{bio_qual}\n")
         print(id)
-        score_dtw, score_ed = evaluate_dtw_ed(read, G, r, qual)
-        csv_output.writerow([id, nb_ids, nb_homopoly, score_dtw, score_ed])
+        # print(r, dele_prec[r])
+        score_dtw, score_ed = evaluate_dtw_ed(
+            read, Go, r, qual, biological_var, bio_qual
+        )
+        csv_output.writerow(
+            [id, biological_var, nb_s, nb_homopoly, score_dtw, score_ed]
+        )
     read_file.close()
     output_file.close()
 
@@ -130,7 +202,7 @@ def parse_args():
         "-N", "--N", help="Number of read per set of parameters", type=int, default=10
     )
     parser.add_argument(
-        "-RL", "--read_length", help="Read length", type=int, default=500
+        "-RL", "--read_length", help="Read length", type=int, default=200
     )
     parser.add_argument(
         "-IDS",
@@ -155,14 +227,19 @@ def load_genome(genome_file):
 
 def main():
     err_inser_del_sub_list = [round(0.01 * i, 2) for i in range(11)]
-    # err_homopoly_list = [round(0.05 + 0.05 * i, 2) for i in range(6)]
-    err_homopoly_list = [0.2]
+    err_homopoly_list = [round(0.05 + 0.05 * i, 2) for i in range(6)]
+    # err_homopoly_list = [0.1]
     genome_file, N, read_length, ids = parse_args()
-    G = load_genome(genome_file)
-    for err_ed in err_inser_del_sub_list:
-        for err_hom in err_homopoly_list:
-            err = Error_rate(err_ed, err_hom, ids)
-            generate_read(G, N, read_length, err)
+    Go = load_genome(genome_file)
+    err = Error_rate(0)
+    Gi, qual_Gi, nb_IDS_Gi = add_IDS_err(Go, err)
+    print(Go[:200])
+    print(Gi[:200])
+    print(qual_Gi[:200])
+    print(f"biological distance overall: {nb_IDS_Gi}/{len(Gi)}")
+    for err_hom in err_homopoly_list:
+        err = Error_rate(err_hom)
+        generate_read(Go, Gi, qual_Gi, N, read_length, err)
 
 
 if __name__ == "__main__":
